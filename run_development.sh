0fe5ef9639530b00c154dfbc550bb5ed185a3306
Odd Einar Hoel <odd.einar.hoel@nav.no>
2020-08-05 13:46:39 +0200

    Change port

--- run_development.sh
+++ run_development.sh
@@ -8,7 +8,7 @@ docker run \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/data:/app/data \
         -v $CURRENT_PATH/resources:/app/resources \
-        -p 8080:8080 \
+        -p 8081:8080 \
         -e DISABLE_PDF_GET=false \
         -it \
         --rm \
