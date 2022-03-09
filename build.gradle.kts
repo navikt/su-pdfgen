a25a14e8bb01dd5fbd7606d848b12d42a6c323b2
Jacob Meidell <jacob.meidell@nav.no>
2022-03-09 10:28:14 +0100

    Legg til test for ny komponent.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,16 +8,15 @@ plugins {
 
 val junitJupiterVersion = "5.8.2"
 val kotestVersion = "5.1.0"
+val handlebarsVersion = "4.2.0"
 
 dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-json:$kotestVersion")
-    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.3")
-    testImplementation("io.kotest:kotest-extensions:$kotestVersion")
+
+    implementation("com.github.jknack:handlebars:$handlebarsVersion")
 }
 
 tasks.withType<Wrapper> {
