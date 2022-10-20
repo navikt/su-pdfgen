3480ec2daadf700690ef1500b1a88bd1ee2c7331
John Andre Hestad <john.andre.hestad@nav.no>
2022-10-20 14:30:46 +0200

    Fix CURRENT_PATH in run_development.sh

--- run_development.sh
+++ run_development.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+CURRENT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker pull navikt/pdfgen:d84f2a198a1b05af62b87ce994f0a23e22260bca
 docker run \
-        -v $CURRENT_PATH/templates:/app/templates \
-        -v $CURRENT_PATH/fonts:/app/fonts \
-        -v $CURRENT_PATH/data:/app/data \
-        -v $CURRENT_PATH/resources:/app/resources \
+        -v "$CURRENT_PATH"/templates:/app/templates \
+        -v "$CURRENT_PATH"/fonts:/app/fonts \
+        -v "$CURRENT_PATH"/data:/app/data \
+        -v "$CURRENT_PATH"/resources:/app/resources \
         -p 8081:8080 \
         -e DISABLE_PDF_GET=false \
         -it \
