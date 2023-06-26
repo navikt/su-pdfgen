c926d881962171619508d9509db4a2c4ec4b0bf1
John Andre Hestad <john.andre.hestad@nav.no>
2023-06-26 18:07:35 +0200

    Forenkle og tilrettelegg for mac

--- run_development.sh
+++ run_development.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-CURRENT_PATH=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 CONTAINER_NAME="su-pdfgen"
 
-if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-    docker start "$CONTAINER_NAME"
-else
 docker build -t "$CONTAINER_NAME" "$CURRENT_PATH"
+
 docker run \
+        --name "$CONTAINER_NAME" \
         -v "$CURRENT_PATH"/data:/app/data \
         -p 8081:8080 \
         -e DISABLE_PDF_GET=false \
         -it \
+        --rm \
         "$CONTAINER_NAME"
-fi
